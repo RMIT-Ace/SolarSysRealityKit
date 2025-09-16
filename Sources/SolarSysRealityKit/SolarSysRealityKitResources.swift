da4be5f5a85a7a937ceac4862ad5f35b75f53660
RMIT-Ace <ace.authors@rmit.edu.au>
2025-09-16 11:27:52 +1000

    Added helper to expose bundle

---
+++ Sources/SolarSysRealityKit/SolarSysRealityKitResources.swift
@@ -0,0 +1,12 @@
+//
+//  SolarSysRealityKitResources.swift
+//  SolarSysRealityKit
+//
+//  Created by Ace on 16/9/2025.
+//
+import Foundation
+
+// inside SolarSysRealityKit package
+public enum SolarSysRealityKitResources {
+    public static let bundle: Bundle = .module
+}
